5a8fa239517a0261134dc34250b9afaeb38ad3a1
ratmi <rfgeboss@gmail.com>
2025-03-02 19:30:58 +0500

    updated Poizon class

--- poizon/__init__.py
+++ poizon/__init__.py
@@ -1,5 +1,8 @@
 import aiohttp
 
+import entities
+
+
 
 class Poizon:
     BASE_API = 'https://poizon-api.com/api/dewu/'
@@ -21,13 +24,16 @@ class Poizon:
     async def get_product_detail_with_price(self, spu_id: int):
         method = 'productDetailWithPrice'
         res = await self.session.get(method, params={'spuId': spu_id})
-        json = await res.json()
-        print(json)
+        return entities.PoizonProduct.parse_json(await res.json())
 
     async def get_product_detail(self, spu_id: str):
         method = 'productDetail'
         res = await self.session.get(method, params={'spuId': spu_id})
-        json = await res.json()
-        print(json)
+        return entities.PoizonProduct.parse_json(await res.json())
 
-    async def get_price_info
+    async def search_products_v1(self, keyword: str, limit: int = 50, page: int = 0):
+        method = 'searchProducts'
+        response = await self.session.get(method, params={'keyword': keyword,
+                                                          'limit': limit,
+                                                          'page': page})
+        return await response.json()
