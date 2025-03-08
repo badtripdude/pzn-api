9c3a0e681b88be9b5273b33f902a7ba447ad0caa
wavy babie <wavybabie@MacBookAir.local>
2025-03-08 19:11:40 +0500

    added class PoizonCategoriesParser to parse categories, added params category_id and brand_id to class PoizonProduct

--- dewu/entities.py
+++ dewu/entities.py
@@ -3,7 +3,7 @@ import json
 from abc import ABC
 from typing import Optional, List, Dict
 
-
+NON_STATED = None
 # class Product:
 #     id: int
 #     title: str
@@ -20,9 +20,11 @@ class JsonSerializable(ABC):
 
 
 class PoizonProductRaw(JsonSerializable):
-    '''
-    получение сырых данных с пойзона (практически сырой json)
-    '''
+    """
+        получение сырых данных с пойзона (практически сырой json)
+
+    """
+
 
     def __init__(self, price, detail, frontLabelSummaryDTO, lastSold, image, spuGroupList, saleProperties, basicParam,
                  favoriteData, brandRootInfo, sizeDto, relateProductInfo, shareInfo, skus):
@@ -65,48 +67,66 @@ class PoizonProduct(JsonSerializable):
     '''
     класс для хранения уже распарсенных данных
     '''
-
-    def __init__(self, product_params, articul, category, current_sizes, current_colors, size_ids, color_ids, prices,
-                 sku_ids, spu_id, floor_price,
-                 current_images, brand, brand_logo, title, desc, size_table):
-        self.product_params: Dict = product_params
-        self.articul = articul
-        self.category: str = category
-        self.current_sizes: List[int] = current_sizes
-        self.current_colors: List[str] = current_colors
-        self.size_ids: Optional[List[int]] = size_ids
-        self.color_ids: Optional[List[str]] = color_ids
-        self.prices: Optional[List[int]] = prices
-        self.sku_ids: List[int] = sku_ids  # уникальный
-        self.spu_id: int = spu_id  # общий
-        self.floor_price: Optional[int] = floor_price
-        self.current_images: List[str] = current_images
-        self.brand: str = brand
-        self.brand_logo: str = brand_logo
-        self.title: str = title
-        self.desc: Optional[str] = desc
-        self.size_table: List[int] = size_table
+    def __init__(self,
+                 product_params: Dict[str, str] | NON_STATED,
+                 article: str,
+                 category: str | NON_STATED,
+                 category_id: int,
+                 current_sizes: List[int] | NON_STATED,
+                 current_colors: List[str] | NON_STATED,
+                 size_ids: List[int] | NON_STATED,
+                 color_ids: List[int] | NON_STATED,
+                 prices: List[int] | NON_STATED,
+                 sku_ids: List[int] | NON_STATED,
+                 spu_id: int,
+                 floor_price: int | NON_STATED,
+                 current_images: List[str],
+                 brand: str,
+                 brand_logo: str,
+                 brand_id: str,
+                 title: str,
+                 desc: str | NON_STATED,
+                 size_table: Dict[str, str] | NON_STATED):
+        self.product_params = product_params
+        self.article = article
+        self.category= category
+        self.category_id = category_id
+        self.current_sizes = current_sizes
+        self.current_colors = current_colors
+        self.size_ids = size_ids
+        self.color_ids = color_ids
+        self.prices = prices
+        self.sku_ids = sku_ids  # уникальный
+        self.spu_id = spu_id  # общий
+        self.floor_price = floor_price
+        self.current_images = current_images
+        self.brand = brand
+        self.brand_logo = brand_logo
+        self.brand_id = brand_id
+        self.title = title
+        self.desc = desc
+        self.size_table = size_table
 
     # картинки соотносятся по цветам не по размерам
     @classmethod
     def from_json(cls, json_data):
         raw_data = PoizonProductRaw.from_json(json_data=json_data)
-        skus, current_colors, current_sizes, prices, sizeIds, colorIds, current_images = ([] for _ in range(7))
+        sku_ids, current_colors, current_sizes, prices, size_ids, color_ids, current_images = ([] for _ in range(7))
         size_table = {}
-        # TODO товары с доставкой через европу убрать из списка, категория айди
+        # TODO товары с доставкой через европу убрать из списка
         for i in raw_data.skus:
             current_images.append(i['logoUrl'])
-            skus.append(i['skuId'])
+            sku_ids.append(i['skuId'])
             if 'price' in i:
-                prices.append(i["price"]["prices"][0]["price"] if i["price"]["prices"] else 0)
+                prices.append(i["price"]["prices"][0]["price"] if i["price"]["prices"] else NON_STATED)
             for j in i["properties"]:
                 if 'saleProperty' in j:
                     if j["saleProperty"]["name"] == "颜色":
                         current_colors.append(j["saleProperty"]["value"])
-                        colorIds.append(j["saleProperty"]["propertyValueId"])
+                        color_ids.append(j["saleProperty"]["propertyValueId"])
                     elif j["saleProperty"]["name"] == "尺码":
                         current_sizes.append(j["saleProperty"]["value"])
-                        sizeIds.append(j["saleProperty"]["propertyValueId"])
+                        size_ids.append(j["saleProperty"]["propertyValueId"])
         if "sizeInfo" in raw_data.sizeDto:
             for i in raw_data.sizeDto["sizeInfo"]["sizeTemplate"]["list"]:
                 size_table[f"{i["sizeKey"]}"] = i["sizeValue"]
@@ -114,27 +134,52 @@ class PoizonProduct(JsonSerializable):
         for i in raw_data.basicParam['basicList']:
             product_params[i["key"]] = i["value"]
 
+        if not product_params: product_params = NON_STATED
+        if not current_sizes: current_sizes = NON_STATED
+        if not current_colors: current_colors = NON_STATED
+        if not size_ids: size_ids = NON_STATED
+        if not color_ids: color_ids = NON_STATED
+        if not prices: prices = NON_STATED
+        if not sku_ids: sku_ids = NON_STATED
+        if not size_table: size_table = NON_STATED
+
         return cls(
             product_params=product_params,
-            articul=raw_data.detail.get('articleNumber', None),
-            category=raw_data.detail["categoryName"],
+            article=raw_data.detail['articleNumber'],
+            category=raw_data.detail.get("categoryName", NON_STATED),
+            category_id=raw_data.detail["categoryId"],
             current_sizes=current_sizes,
             current_colors=current_colors,
-            size_ids=sizeIds,
-            color_ids=colorIds,
+            size_ids=size_ids,
+            color_ids=color_ids,
             prices=prices,
-            sku_ids=skus,
+            sku_ids=sku_ids,
             spu_id=raw_data.detail["spuId"],
             floor_price=raw_data.price["item"]["floorPrice"] if raw_data.price else None,
             current_images=current_images, brand=raw_data.brandRootInfo["brandItemList"][0]["brandName"],
             brand_logo=raw_data.brandRootInfo["brandItemList"][0]["brandLogo"],
-            title=raw_data.detail["title"], desc=raw_data.detail["desc"],
+            brand_id = raw_data.detail["brandId"],
+            title=raw_data.detail["title"], desc=raw_data.detail["desc"] if raw_data.detail["desc"] else NON_STATED,
             size_table=size_table
         )
 
+class PoizonCategoryParser(JsonSerializable):
+    """
+    класс для получения категорий api: GET /getCategories
+    """
+    def __init__(self, categories: Dict[str, str]):
+        self.categories = categories
+
+    @classmethod
+    def from_json(cls, json_data):
+        categories = {}
+        for i in json_data:
+            categories[i["id"]] = i["name"]
+        return cls(categories = categories)
+
 
 if (__name__ == "__main__"):
-    with open('../shoes_pdwp.json', 'r') as f:
+    with open('../jacket.json', 'r') as f:
         dictData = json.load(f)
 
     a = PoizonProduct.from_json(json_data=dictData)
@@ -144,15 +189,17 @@ if (__name__ == "__main__"):
     print(f"current_sizes = {a.current_sizes}")
     print(f"current_colors = {a.current_colors}")
     print(f"prices = {a.prices}\n")
-
     print(f"brand = {a.brand}")
+    print(f"brand_logo = {a.brand_logo}")
+
+    print(f"brand_id = {a.brand_id}")
     print(f"spu_id = {a.spu_id}")
     print(f"floor_price = {a.floor_price}")
-    print(f"brand_logo = {a.brand_logo}")
     print(f"title = {a.title}")
     print(f"desc = {a.desc}")
     print(f"current_images = {a.current_images}")
     print(f"category = {a.category}")
+    print(f"category_id = {a.category_id}")
     print(f"size_table = {a.size_table}")
     print(f"product_params = {a.product_params}")
-    print(f"articul = {a.articul}")
+    print(f"article = {a.article}")
