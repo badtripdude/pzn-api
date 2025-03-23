18783d83c1c7ed24931d5ba51af009e20222fbf3
wavy babie <wavybabie@MacBookAir.local>
2025-03-23 21:49:37 +0500

    rearranged PoizonProduct params

--- dewu/entities.py
+++ dewu/entities.py
@@ -1,7 +1,6 @@
-import json
 from typing import List, Dict
 from base import JsonSerializable
-from poizon_parse_helpers import ParseSizes, ParseColors, ParseProductIds, ParseProductProperties, ParseBrandInfo, ParsePriceInfo, ParseImages
+from poizon_parse_helpers import ParseBrand, ParseSizeTable, ParseStock, ParseProductCore, PoizonProductRaw, Images
 from raw_data_handlers import PoizonProductRaw
 from base import NON_STATED
 import dataclasses
@@ -15,169 +14,153 @@ import dataclasses
 
 
 
-
-
-
-
 class PoizonProduct(JsonSerializable):
     """
             class for parsing and keeping data from PoizonProductRaw
     """
-    # TODO: dataclasses
-    @dataclasses.dataclass()
-    class PoizonProductPrice:  # todo есть еще описание(не всегда) каждого вида цены но хз надо ли
-        recommended_prices: List[int]
-        types_of_prices: Dict[int, Dict[int, int]] | NON_STATED
-        floor_price: int
-        max_price: int
+    # # TODO: dataclasses оставить ~
+    # @dataclasses.dataclass()
+    # class PoizonProductPrice:  # todo есть еще описание(не всегда) каждого вида цены но хз надо ли
+    #     recommended_prices: List[int]
+    #     types_of_prices: Dict[int, Dict[int, int]] | NON_STATED
+    #     floor_price: int | NON_STATED
+    #     max_price: int
+    #
+    # @dataclasses.dataclass()
+    # class PoizonProductSize:
+    #     current_sizes: Dict[int, str]
+    #     size_table: Dict[str, str]
+    #
+    # @dataclasses.dataclass()
+    # class PoizonProductImages:
+    #     general_logo_image: str
+    #     current_images: Dict[int, List[str]]
+    #
+    # @dataclasses.dataclass()
+    # class PoizonProductDescription:
+    #     product_addictive_params: Dict[str, str]
+    #     category: str
+    #     title: str
+    #     desc: str | NON_STATED
+    #
+    # @dataclasses.dataclass()
+    # class PoizonProductIdentifiers:
+    #     color_ids: Dict[int, int]
+    #     sku_ids: List[int]
+    #     spu_id: int
+    #     article_number: int
+    #     category_id: int
+    #     brand_id: int
+    #     size_ids: Dict[int, str]
+    #
+    # @dataclasses.dataclass()
+    # class PoizonProductColors:
+    #     sku_to_color: Dict[int, str]
+    #
+    # @dataclasses.dataclass()
+    # class PoizonProductBrandInfo:
+    #     brand_name: str
+    #     brand_logo: str
+    #
+    # #todo delivery duration
+    # @dataclasses.dataclass()
+    # class PoizonProductDelivery:
+    #     ...
+    # # 3 -eu
+    # # 2 - ch
+    # # 95 - 95
+    # # 2 - ch
+    # # 0 - ch
+
+    @dataclasses.dataclass
+    class PoizonProductCore:
+        additional_params: Dict[str, str]
+        category: str
+        category_id: int
+        title: str
+        description: str
+        article_number: int
+        spu_id: str
 
     @dataclasses.dataclass()
-    class PoizonProductSize:
-        current_sizes: Dict[int, str]
-        size_table: Dict[str, str]
+    class PoizonProductStock:
+        recommended_prices: List[int]
+        types_of_prices: Dict[int, Dict[int, int]] | NON_STATED
+        types_of_prices_desc: str
+        floor_price: int | NON_STATED
+        max_price: int
+        stock: int
+        sku_ids: List[int]
+
+    @dataclasses.dataclass()
+    class PoizonProductSizeTable:
+        size_table: List[int] | NON_STATED
 
     @dataclasses.dataclass()
     class PoizonProductImages:
-        general_logo_image: str
-        current_images: Dict[int, List[str]]
+        general_logo_url: str
+        sku_to_image_url: Dict[int, List[str]]
 
     @dataclasses.dataclass()
-    class PoizonProductDescription:
-        product_addictive_params: Dict[str, str]
-        category: str
-        title: str
-        desc: str | NON_STATED
-
-    @dataclasses.dataclass()
-    class PoizonProductIds:
-        color_ids: Dict[int, int]
-        sku_ids: List[int]
-        spu_id: int
-        article_number: int
-        category_id: int
-        brand_id: int
-        size_ids: Dict[int, str]
-
-    @dataclasses.dataclass()
-    class PoizonProductColors:
-        sku_to_color: Dict[int, str]
-
-    @dataclasses.dataclass()
-    class PoizonProductBrandInfo:
+    class PoizonProductBrand:
         brand_name: str
-        brand_logo: str
-
-    #todo delivery duration
-    @dataclasses.dataclass()
-    class PoizonProductDelivery:
-        ...
+        brand_logo_url: str
+        brand_id: int
 
     def __init__(self,
-                 sizes: PoizonProductSize,
-                 descriptions: PoizonProductDescription,
-                 prices: PoizonProductPrice | NON_STATED,
-                 images: PoizonProductImages | NON_STATED,
-                 product_ids: PoizonProductIds,
-                 colors: PoizonProductColors,
-                 brand_info: PoizonProductBrandInfo,
+
+                 core: PoizonProductCore,
+                 stock: PoizonProductStock,
+                 size_table: PoizonProductSizeTable,
+                 images: PoizonProductImages,
+                 brand: PoizonProductBrand
                  ):
+
+        self.core = core
+        self.stock = stock
+        self.size_table = size_table
         self.images = images
-        self.product_ids = product_ids
-        self.descriptions = descriptions
-        self.prices = prices
-        self.colors = colors
-        self.sizes = sizes
-        self.brand_info = brand_info
+        self.brand = brand
 
     # картинки соотносятся по цветам не по размерам
     @classmethod
     def from_json(cls, json_data):
         raw_data = PoizonProductRaw.from_json(json_data=json_data)
-        #sizes
-        parsed_sizes = ParseSizes.from_json(raw_data=raw_data)
-        sizes = cls.PoizonProductSize(current_sizes=parsed_sizes.current_sizes,size_table=parsed_sizes.size_table)
-        #colors
-        parsed_colors = ParseColors.from_json(raw_data=raw_data)
-        colors = cls.PoizonProductColors(sku_to_color=parsed_colors.sku_to_color)
-        #product ids
-        parsed_ids = ParseProductIds.from_json(raw_data=raw_data)
-        product_ids = cls.PoizonProductIds(color_ids=parsed_ids.color_ids, sku_ids=parsed_ids.sku_ids,
-                                            spu_id=parsed_ids.spu_id, article_number=parsed_ids.article_number,
-                                            category_id=parsed_ids.category_id, brand_id=parsed_ids.brand_id,
-                                            size_ids=parsed_ids.size_ids)
-        #desc_info
-        parsed_desc = ParseProductProperties.from_json(raw_data=raw_data)
-        descriptions = cls.PoizonProductDescription(product_addictive_params=parsed_desc.product_addictive_params, category=parsed_desc.category,
-                                                              title=parsed_desc.title, desc=parsed_desc.desc)
-        #brand info
-        parsed_brand_info = ParseBrandInfo.from_json(raw_data=raw_data)
-        brand_info = cls.PoizonProductBrandInfo(brand_name=parsed_brand_info.brand_name, brand_logo=parsed_brand_info.brand_logo)
-        #price info
-        parsed_prices = ParsePriceInfo.from_json(raw_data=raw_data)
-        prices = cls.PoizonProductPrice(recommended_prices=parsed_prices.recommended_prices, types_of_prices=parsed_prices.types_of_prices,
-                           floor_price=parsed_prices.floor_price, max_price=parsed_prices.max_price)
-        #imgs 3 positions
-        parsed_images = ParseImages.from_json(raw_data=raw_data)
-        images = cls.PoizonProductImages(general_logo_image=parsed_images.general_logo_image, current_images=parsed_images.current_images)
+
+        parsed_core = ParseProductCore.from_json(raw_data=raw_data)
+        core = cls.PoizonProductCore(additional_params=parsed_core.additional_params,
+                                     category=parsed_core.category,
+                                     category_id=parsed_core.category_id,
+                                     title=parsed_core.title,
+                                     description=parsed_core.description,
+                                     article_number=parsed_core.article_number,
+                                     spu_id=parsed_core.spu_id)
+
+        parsed_stock = ParseStock.from_json(raw_data=raw_data)
+        stock = cls.PoizonProductStock(recommended_prices=parsed_stock.recommended_prices,
+                                       types_of_prices=parsed_stock.types_of_prices,
+                                       types_of_prices_desc=parsed_stock.types_of_prices_desc,
+                                       floor_price=parsed_stock.floor_price,
+                                       max_price=parsed_stock.max_price,
+                                       stock=parsed_stock.stock,
+                                       sku_ids=parsed_stock.sku_ids)
+
+        parsed_size_table = ParseSizeTable.from_json(raw_data=raw_data)
+        size_table = cls.PoizonProductSizeTable(size_table=parsed_size_table.size_table)
+
+        parsed_images = Images.from_json(raw_data=raw_data)
+        images = cls.PoizonProductImages(general_logo_url=parsed_images.general_logo_url,
+                                         sku_to_image_url=parsed_images.sku_to_image_url)
+
+        parsed_brand = ParseBrand.from_json(raw_data=raw_data)
+        brand = cls.PoizonProductBrand(brand_name=parsed_brand.brand_name,
+                                       brand_logo_url=parsed_brand.brand_logo_url,
+                                       brand_id=parsed_brand.brand_id)
 
         return cls(
-            sizes=sizes,
-            product_ids=product_ids,
-            descriptions=descriptions,
-            colors=colors,
-            brand_info=brand_info,
-            prices=prices,
-            images=images
+            core=core,
+            stock=stock,
+            size_table=size_table,
+            images=images,
+            brand=brand
         )
-
-
-
-if (__name__ == "__main__"):
-    with open('../shoes_pdwp.json', 'r') as f:
-        dictData = json.load(f)
-
-    a = PoizonProduct.from_json(json_data=dictData)
-
-
-
-    print("\n### PRICES ###")
-    print(f"types_of_prices = {a.prices.types_of_prices}")
-    print(f'recommended_prices = {a.prices.recommended_prices}')
-    print(f'floor_price = {a.prices.floor_price}')
-    print(f'max_price = {a.prices.max_price}')
-
-    print("\n### SIZES ###")
-    print(f'current_sizes = {a.sizes.current_sizes}')
-    print(f'size_table = {a.sizes.size_table}')
-
-    print("\n### IMAGES ###")
-    print(f'current_images = {a.images.current_images}')
-    print(f'general_logo_image = {a.images.general_logo_image}')
-
-    print(f'\n### DESCRIPTIONS ###')
-    print(f'product_addictive_params = {a.descriptions.product_addictive_params}')
-    print(f'category = {a.descriptions.category}')
-    print(f'desc = {a.descriptions.desc}')
-    print(f'title = {a.descriptions.title}')
-
-    print(f'\n### IDs ###')
-    print(f'color_ids = {a.product_ids.color_ids}')
-    print(f'sku_ids = {a.product_ids.sku_ids}')
-    print(f'spu_id = {a.product_ids.spu_id}')
-    print(f'article_number = {a.product_ids.article_number}')
-    print(f'category_id = {a.product_ids.category_id}')
-    print(f'brand_id = {a.product_ids.brand_id}')
-    print(f'size_ids = {a.product_ids.size_ids}')
-
-    print(f'\n### COLORS ###')
-    print(f'colors = {a.colors.sku_to_color}')
-
-    print(f'\n### BRAND INFO ###')
-    print(f'brand info = {a.brand_info.brand_name}')
-    print(f'brand_logo = {a.brand_info.brand_logo}')
-
-
-    for item in dictData['skus']:
-        for price in item['price']['prices']:
-            print(f'{price['tradeType']} {price['timeDelivery']} {price['tradeDesc']}')
-
